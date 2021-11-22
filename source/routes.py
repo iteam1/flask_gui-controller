aa9b2d98aaf03847f119fcbc98ceab6ae05fb2d5
locchuong <locchuong123@gmail.com>
2021-11-22 17:59:28 +0700

    done

--- source/routes.py
+++ source/routes.py
@@ -1,7 +1,32 @@
-from source import app 
+from source import app,db
+from source.models import Showman 
+from flask import render_template,request
 
-@app.route('/')
+@app.route('/',methods =['GET'])
 def home():
-	return 'Hello World!'
+	return render_template('helm.html')
+
+@app.route('/content',methods =['GET','POST'])
+def content():
+	if request.method == 'POST':
+		content = request.form.get('content')
+		showman = Showman.query.get(1)
+		showman.itype = 'info'
+		showman.content = content
+		db.session.commit()
+		return render_template('helm.html')
+	return render_template('helm.html')
+
+@app.route('/emotion',methods =['GET','POST'])
+def emotion():
+	if request.method == 'POST':
+		emotion = request.form.get('emotion')
+		showman = Showman.query.get(1)
+		print(emotion)
+		showman.itype = 'emo'
+		showman.emotion = emotion
+		db.session.commit()
+		return render_template('helm.html')
+	return render_template('helm.html')
 
 	
